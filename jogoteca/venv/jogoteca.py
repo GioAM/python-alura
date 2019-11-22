26c28fd23a3588140910d2e2c91c186aa140e0e2
Giovani Meneguel <34317514+GioAM@users.noreply.github.com>
2019-11-21 21:19:12 -0300

    TOP

--- jogoteca/venv/jogoteca.py
+++ jogoteca/venv/jogoteca.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, session, url_for
+from dao import JogoDao, UsuarioDao
+from models import Jogo, Usuario
+from flask_mysqldb import MySQL
+db = MySQL(app)
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
-class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
+app.config['MYSQL_HOST'] = "0.0.0.0"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = "admin"
+app.config['MYSQL_DB'] = "jogoteca"
+app.config['MYSQL_PORT'] = 3306
 
-jogo1 = Jogo('Super Mario', 'Acao', 'SNES')
-jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-jogo3 = Jogo('Mortal Kombat', 'LutA', 'SNES')
-lista = [jogo1, jogo2, jogo3]
+db = MySQL(app)
+
+jogo_dao = JogoDao(db)
+usuario_dao = UsuarioDao(db)
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='Jogos',
-                           jogos=lista)
+    lista = jogo_dao.listar()
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect("/login?proxima=novo")
     return render_template('novo.html', titulo="Jogos");
 
 
@@ -29,21 +38,36 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect("/")
+    return redirect(url_for('index'))
+
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'mestra' == request.form['senha']:
-        return redirect('/')
-    else :
-        return redirect ('/login')
+    usuario = usuario_dao.buscar_por_id(request.form['usuario'])
+    if usuario:
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)pip3.exe install mysqlclient
+        else:
+            flash('Não logado, tente de novo!')
+            return redirect(url_for('login'))
+    else:
+        flash('Não logado, tente de novo!')
+        return redirect(url_for('login'))
 
-    
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect(url_for('index'))
+
 app.run(debug=True)
 
-
-.
